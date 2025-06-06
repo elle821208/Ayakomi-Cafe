af44efb5b3286e93e1b5e3b931a3838dcdddb0ef
tetsuya masano <elle.821208@gmail.com>
2025-06-06 18:12:19 +0900

    最初のコミット

---
+++ .history/index_20250419071802.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>AyakomiCafe</title>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/common.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page.css">
+    </head>
+    <body>
+        <!--https://www.youtube.com/watch?v=D6aeJTnGvHo-->
+        <!--HTMLCSS基礎プラス実践で使える技術盛込みパワーアップ2024-->
+        <!--https://www.pakutaso.com/20230807241post-48316.html-->
+        <!--ぱくたそ-->
+        <!--https://icooon-mono.com/-->
+        <!--アイコン-->
+        <!--1:22:30  -MENU-->
+        <header>
+            <div class="container">
+                <h1>AyakomiCafe</h1>
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Menu</a></li>
+                        <li><a href="">Access</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+
+        </header>
+
+        <main>
+            <section class="hero">
+                <img src="<?php echo get_template_directory_uri();/*これでimg画像を読み込む*/ ?>/images/narantoolzaha_baiten.jpg" alt="飲食スペース">
+                <div class="container">
+
+                </div>
+
+            </section>
+
+            <section class="about" >
+                <div class="container">
+                    <h2>ABOUT</h2>
+                    <p>サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                        サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</p>
+
+                </div>
+
+            </section>
+
+            <section class="menu">
+                <div class="container">
+                    <h2>MENU</h2>
+                    <table>
+                        <tr>
+                            <td>サンプルサンプルメニュー</td>
+                            <td>400円</td>
+                        </tr>
+                        <tr>
+                            <td>サンプルメニュー</td>
+                            <td>400円</td>
+                        </tr>
+                        <tr>
+                            <td>サンプルサンプルメニュー</td>
+                            <td>400円</td>
+                        </tr>
+                        <tr>
+                            <td>サンプルメニュー</td>
+                            <td>400円</td>
+                        </tr>
+                        <tr>
+                            <td>サンプルサンプルメニュー</td>
+                            <td>400円</td>
+                        </tr>
+                        <tr>
+                            <td>サンプルメニュー</td>
+                            <td>400円</td>
+                        </tr>
+                        <tr>
+                            <td>サンプルサンプルメニュー</td>
+                            <td>400円</td>
+                        </tr>   
+                    </table>
+
+
+                </div>
+
+            </section>
+
+        </main>
+
+        <aside>
+            <div class="container">
+                <h2>INFORMATION</h2>
+                <p>サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル
+                    サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル</p>
+                <figure>
+                    <img src="assets/images/hoseki_AI.ｊg" alt="カフェ">
+                    <div class="btn">
+                        <a href="">ボタン</a>
+                    </div>
+                </figure>
+
+
+            </div>
+
+        </aside>
+
+        <footer>
+            <div class="container">
+                <small>&copy;AyakomiCafe</small>
+
+            </div>
+
+        </footer>
+
+    </body>
+</html>
